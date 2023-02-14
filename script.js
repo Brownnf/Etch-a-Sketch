e2ba93bf0b86ac0a100ac1fe170f5a8443b17dca
Brownnf <65924443+Brownnf@users.noreply.github.com>
2023-02-14 18:25:03 -0500

    Add function to change board column ratio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //build board
 function buildBoard(size) {
+  columnRatio(size);
   size = size * size;
   for (i = 0; i < size; i++) {
     let tile = document.createElement("div");
@@ -7,6 +8,13 @@ function buildBoard(size) {
     //Append divs to "board" div
     document.getElementById("board").appendChild(tile);
   }
+}
+
+//Function to change the ratio for the grid template
+function columnRatio(size) {
+  let board = document.getElementById("board");
+  let percentage = 100 / size;
+  board.style.gridTemplateColumns = `repeat(${size}, ${percentage}%);`;
 }
 //Listen for mouse click on board
 //When click held, add class to divs inside of "board" div
