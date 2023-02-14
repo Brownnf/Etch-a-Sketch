92433618ab1b2bd1beb5e816218de73d611755d6
Brownnf <65924443+Brownnf@users.noreply.github.com>
2023-02-14 16:29:30 -0500

    Add basic outline for webpage

---
+++ script.js
@@ -0,0 +1,6 @@
+//build board
+//Create x amount of divs with "tile" class with for loop
+//Append divs to "board" div
+//Listen for mouse click on board
+//When click held, add class to divs inside of "board" div
+//Listen for button clicks to change options
