af2b2cb1fdb820bdf5b9de918304fa585e57b479
Brownnf <65924443+Brownnf@users.noreply.github.com>
2023-02-14 17:01:58 -0500

    Add function to build board

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //build board
-//Create x amount of divs with "tile" class with for loop
-//Append divs to "board" div
+function buildBoard(size) {
+  size = size * size;
+  for (i = 0; i < size; i++) {
+    let tile = document.createElement("div");
+    tile.className = "tile";
+    //Append divs to "board" div
+    document.getElementById("board").appendChild(tile);
+  }
+}
 //Listen for mouse click on board
 //When click held, add class to divs inside of "board" div
 //Listen for button clicks to change options
