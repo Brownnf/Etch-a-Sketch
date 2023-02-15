8af1405f4af4422bd716f0f4fc63fe9dd749de44
Brownnf <65924443+Brownnf@users.noreply.github.com>
2023-02-15 02:28:16 -0500

    Add buttons to index and listeners to script

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //build board
 function buildBoard(size) {
   columnRatio(size);
+  //Clear out any previous divs
+  let elements = document.getElementsByClassName("tile");
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
   size = size * size;
   for (i = 0; i < size; i++) {
     let tile = document.createElement("div");
@@ -13,10 +18,20 @@ function buildBoard(size) {
 //Function to change the ratio for the grid template
 function columnRatio(size) {
   let board = document.getElementById("board");
-  let percentage =`repeat(${size}, ${100 / size}%)`;
+  let percentage = `repeat(${size}, ${100 / size}%)`;
   board.style.gridTemplateColumns = percentage;
   board.style.gridTemplateRows = percentage;
 }
 //Listen for mouse click on board
 //When click held, add class to divs inside of "board" div
 //Listen for button clicks to change options
+
+var rangeInput = document.getElementById("myRange");
+
+rangeInput.addEventListener("mouseup", function () {
+  if (this.value > 0 && this.value < 0) {
+    alert("First");
+  } else {
+    buildBoard(this.value);
+  }
+});
