daab76a3d457d2091a9b9cbbf5cd8496150b50c1
Brownnf <65924443+Brownnf@users.noreply.github.com>
2023-02-17 17:30:05 -0500

    Add rainbow option and refactor clear function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //build board
+let bsize = 0;
 function buildBoard(size) {
+  bsize = size;
   columnRatio(size);
   //Clear out any previous divs
   let elements = document.getElementsByClassName("tile");
@@ -23,55 +25,69 @@ function columnRatio(size) {
   board.style.gridTemplateRows = percentage;
 }
 
+let color = "black";
+let standard = true;
+
+//function to get user color
+function colorChoice(choice) {
+  if (choice == "rainbow") {
+    color = ["#3C9EE7", "#E7993C", "#E73C99", "#3CE746", "#E7993C"];
+    standard = false;
+    buildBoard(bsize);
+  } else if (choice !== "black") {
+    color = choice;
+    standard = false;
+    buildBoard(bsize);
+  } else {
+    standard = true;
+  }
+}
+
 //Get sketch board
-let divArray = document.getElementById('board');
+let divArray = document.getElementById("board");
 
 //Observe the board, and add the event listeners once the divs have been populated
-let observer = new MutationObserver(function(){
-   let tiles = document.getElementsByClassName('tile');
-   let tileArray = Array.from(tiles);
-   tileArray.forEach((tileArray) => {
-    tileArray.addEventListener('mouseover', () => {tileArray.classList.add("black");
-    });
+let observer = new MutationObserver(function () {
+  let tiles = document.getElementsByClassName("tile");
+  let tileArray = Array.from(tiles);
+  tileArray.forEach((tileArray) => {
+    if (standard == true) {
+      tileArray.addEventListener("mouseover", () => {
+        tileArray.style.background = color;
+      });
+    } else if (standard == false) {
+      tileArray.addEventListener("mouseover", () => {
+        tileArray.style.background = color[Math.floor(Math.random() * color.length)];
+      });
+    }
   });
 });
 observer.observe(divArray, { attributes: false, childList: true, subtree: true });
 
 //Clear board function
-function clearboard(){
-  //Get tiles that have been changed
-  let board = document.getElementById("board").querySelectorAll(".black");
-  //Build array
-  let boardArray = Array.from(board);
-  //Clear selected divs of their "black" class
-  boardArray.forEach(element => {
-    element.classList.remove("black");
-    
-  });
+function clearBoard() {
+  buildBoard(bsize);
 }
 
 //Toggle gridlines function
-function gridToggle(){
-    //Get board
-    let board = document.getElementById("board").querySelectorAll(".tile");
-    //Build array
-    let boardArray = Array.from(board);
-      //Toggle grid class on all the divs
-      boardArray.forEach(element => {
-        element.classList.toggle("grid");
-        
-      });
+function gridToggle() {
+  //Get board
+  let board = document.getElementById("board").querySelectorAll(".tile");
+  //Build array
+  let boardArray = Array.from(board);
+  //Toggle grid class on all the divs
+  boardArray.forEach((element) => {
+    element.classList.toggle("grid");
+  });
 }
-
 
 //Listen for button clicks to change options
 
 let rangeInput = document.getElementById("myRange");
 let boardSize = document.getElementById("boardSize");
 
-rangeInput.addEventListener("input", function(){
-
-  boardSize.textContent = `${this.value}x${this.value}`
+rangeInput.addEventListener("input", function () {
+  boardSize.textContent = `${this.value}x${this.value}`;
 });
 
 rangeInput.addEventListener("mouseup", function () {
